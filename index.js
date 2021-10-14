f6d9aa9139f8d0fea7762e85b3e40f6e1f1b8b0b
Rivka-Fogelman <76700130+Rivka-Fogelman@users.noreply.github.com>
2021-10-14 15:33:07 +0300

    add cors module to the server

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const routerApi = require('./routes/api')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 
 
 const app = express();
+app.use(cors())
+
 dotenv.config();
 mongoose.connect(process.env.MONGO_CONNECTION,
   {
